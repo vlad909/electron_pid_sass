c7e506245cc625a68923396025d9d1bab7a585cc
VLADISLAV GUSHCHIN <vlad-nighthunter@mail.ru>
2018-10-10 23:19:45 +0300

    vuex with mapfields and custom mutations

--- src/renderer/components/MainNavigationElements/ModifiedHeader.vue
+++ src/renderer/components/MainNavigationElements/ModifiedHeader.vue
@@ -4,69 +4,69 @@
             <div class="d-flex justify-content-start m-t-10 flex-column m-r-10">
                 <div class="form-group d-inline-flex text-left">
                     <label class="m-l-r-10 label-standart-15" for="L">L</label>
-                    <input type="text" class="form-control limiter-input" id="L" v-model.number="experiment.L"
-                           :readonly="!experiment.LRBE">
+                    <input type="text" class="form-control limiter-input" id="L" v-model.number="L"
+                           :readonly="!LRBE">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-15" for="L">R</label>
-                    <input type="text" class="form-control limiter-input" id="R" v-model.number="experiment.R"
-                           :readonly="!experiment.LRBE">
+                    <input type="text" class="form-control limiter-input" id="R" v-model.number="R"
+                           :readonly="!LRBE">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-15" for="betta">β</label>
-                    <input type="text" class="form-control limiter-input" id="betta" v-model.number="experiment.Betta"
-                           :readonly="!experiment.LRBE">
+                    <input type="text" class="form-control limiter-input" id="betta" v-model.number="Betta"
+                           :readonly="!LRBE">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-15" for="e">E</label>
-                    <input type="text" class="form-control limiter-input" id="e" v-model.number="experiment.E"
-                           :readonly="!experiment.LRBE">
+                    <input type="text" class="form-control limiter-input" id="e" v-model.number="E"
+                           :readonly="!LRBE">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-15" for="a">a</label>
-                    <input type="text" class="form-control limiter-input" id="a" v-model.number="experiment.a"
-                           :readonly="experiment.LRBE">
+                    <input type="text" class="form-control limiter-input" id="a" v-model.number="a"
+                           :readonly="LRBE">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-15" for="b">b</label>
-                    <input type="text" class="form-control limiter-input" id="b" v-model.number="experiment.b"
-                           :readonly="experiment.LRBE">
+                    <input type="text" class="form-control limiter-input" id="b" v-model.number="b"
+                           :readonly="LRBE">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-15" for="n">n</label>
-                    <input type="text" class="form-control limiter-input" id="n" v-model.number="experiment.n">
+                    <input type="text" class="form-control limiter-input" id="n" v-model.number="n">
                 </div>
             </div>
             <div class="d-flex justify-content-start m-t-10 flex-column">
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-20" for="Kp">Kp</label>
-                    <input type="text" class="form-control limiter-input" id="Kp" v-model.number="experiment.Kp">
+                    <input type="text" class="form-control limiter-input" id="Kp" v-model.number="Kp">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-20" for="Ki">Ki</label>
-                    <input type="text" class="form-control limiter-input" id="Ki" v-model.number="experiment.Ki">
+                    <input type="text" class="form-control limiter-input" id="Ki" v-model.number="Ki">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-20" for="Kd">Kd</label>
-                    <input type="text" class="form-control limiter-input" id="Kd" v-model.number="experiment.Kd">
+                    <input type="text" class="form-control limiter-input" id="Kd" v-model.number="Kd">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-r-10 label-standart-20" for="bigT">T</label>
-                    <input type="text" class="form-control limiter-input" id="bigT" v-model.number="experiment.T">
+                    <input type="text" class="form-control limiter-input" id="bigT" v-model.number="T">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-10" for="ap">a(p)</label>
-                    <input type="text" class="form-control limiter-input m-l-5" id="ap" v-model.number="experiment.ap"
-                           :readonly="!experiment.LRBE">
+                    <input type="text" class="form-control limiter-input m-l-5" id="ap" v-model.number="ap"
+                           :readonly="!LRBE">
                 </div>
                 <div class="form-group d-inline-flex">
                     <label class="m-l-10" for="bp">b(p)</label>
-                    <input type="text" class="form-control limiter-input m-l-5" id="bp" v-model.number="experiment.bp"
-                           :readonly="!experiment.LRBE">
+                    <input type="text" class="form-control limiter-input m-l-5" id="bp" v-model.number="bp"
+                           :readonly="!LRBE">
                 </div>
                 <div class="form-group d-inline-flex" style="margin-top: -7px;">
                     <label class="m-l-r-10 label-standart-20" for="smallt">t</label>
-                    <input type="text" class="form-control limiter-input" id="smallt" v-model.number="experiment.t">
+                    <input type="text" class="form-control limiter-input" id="smallt" v-model.number="t">
                 </div>
             </div>
             <div class="m-t-10 m-l-20 d-flex justify-content-start flex-column align-items-center">
@@ -114,12 +114,12 @@
         </div>
         <div class="form-group d-inline-flex">
             <label class="m-l-r-10 label-standart-15" for="x0">x0</label>
-            <input type="text" class="form-control limiter-input-extended" id="x0" v-model="experiment.x0"
+            <input type="text" class="form-control limiter-input-extended" id="x0" v-model="x0"
                    placeholder="Enter value or formula">
         </div>
         <div class="d-flex m-l-10 m-b-10">
             <div class="form-group form-check m-t-20">
-                <input type="checkbox" class="form-check-input" id="LRBE" v-model="experiment.LRBE">
+                <input type="checkbox" class="form-check-input" id="LRBE" v-model="LRBE">
                 <label class="form-check-label" for="LRBE">Model LRβE</label>
             </div>
             <button type="button" class="btn btn-success m-l-20" title="Copy data to old method">Swap params</button>
@@ -130,30 +130,28 @@
 <script>
     //    import {actions} from '../../assets/js/globalMixin'
     import mixedComponent from './MixedParams.vue'
+    import {mapFields} from 'vuex-map-fields';
     export default{
         components: {mixedComponent},
         data(){
             return {
-//                experiment: {
-//                    LRBE: false,
-//                    L: 1.3,
-//                    R: 3.4,
-//                    Betta: 1,
-//                    E: 5,
-//                    a: 0.9,
-//                    b: 0.1,
-//                    ap: 0.27,
-//                    bp: 0.27,
-//                    n: 100,
-//                    x0: 5,
-//                    Kp: 10,
-//                    Ki: 1,
-//                    Kd: 3,
-//                    T: 1,
-//                    t: 100,
-//                    formula: 'P'
-//                },
-                selected_formula: 'P',
+//                LRBE: false,
+//                L: 1.3,
+//                R: 3.4,
+//                Betta: 1,
+//                E: 5,
+//                a: 0.9,
+//                b: 0.1,
+//                ap: 0.27,
+//                bp: 0.27,
+//                n: 100,
+//                x0: 5,
+//                Kp: 10,
+//                Ki: 1,
+//                Kd: 3,
+//                T: 1,
+//                t: 100,
+//                selected_formula: 'P',
                 formulas: [
                     {text: 'Rectangle', value: 'P'},
                     {text: 'Trapeze', value: 'T'},
@@ -163,26 +161,33 @@
                 ]
             }
         },
-        computed:{
-            experiment:{/*TODO https://markus.oberlehner.net/blog/form-fields-two-way-data-binding-and-vuex/
-            исрпваить ошибку vuex handling form
-            */
-                get () {
-                    return this.$store.state.lastExperiment
-                },
-                set (value) {
-                    this.$store.commit('toLastExperiment', value)
-                }
-            }
+        computed: {
+            ...mapFields([
+                'lastExperiment.LRBE',
+                'lastExperiment.L',
+                'lastExperiment.R',
+                'lastExperiment.Betta',
+                'lastExperiment.E',
+                'lastExperiment.a',
+                'lastExperiment.b',
+                'lastExperiment.ap',
+                'lastExperiment.bp',
+                'lastExperiment.n',
+                'lastExperiment.x0',
+                'lastExperiment.Kp',
+                'lastExperiment.Ki',
+                'lastExperiment.Kd',
+                'lastExperiment.T',
+                'lastExperiment.t',
+                'lastExperiment.selected_formula'
+            ])
         },
         watch: {
             experiment(n){
                 console.log(n, 'changed')
             }
         },
-        mounted(){
-//            this.$store.commit('toLastExperiment', this.experiment)
-        }
+        methods: {}
     }
 </script>
 <style scoped>
--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -1,17 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import mutations from './mutations.js'
+import {getField, updateField} from 'vuex-map-fields';
 
 Vue.use(Vuex)
 const state = {
     allExperiments: [],
     lastExperiment: {
-
+        LRBE: false,
+        L: 1.3,
+        R: 3.4,
+        Betta: 1,
+        E: 5,
+        a: 0.9,
+        b: 0.1,
+        ap: 0.27,
+        bp: 0.27,
+        n: 100,
+        x0: 5,
+        Kp: 10,
+        Ki: 1,
+        Kd: 3,
+        T: 1,
+        t: 100,
+        selected_formula: 'P'
     }
 }
 
 export default new Vuex.Store({
     strict: process.env.NODE_ENV !== 'production',
     state,
-    mutations
+    mutations: {
+        ...mutations,
+        updateField
+    },
+    getters: {
+        getField
+    }
 })
